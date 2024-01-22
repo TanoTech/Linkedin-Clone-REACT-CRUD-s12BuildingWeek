10b900234c59da31918ee7adbddfd51b89c4b740
GaetanoNapoli <gaetano.napoli@hotmail.com>
2024-01-22 15:51:15 +0100

    fetch dati

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import NavbarTop from './components/Navbar';
+import UserProfile from './components/UserProfile';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavbarTop />
+      <UserProfile />
     </div>
   );
 }
---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserProfile = () => {
+    const [profile, setProfile] = useState(null);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            const endpoint = 'https://striveschool-api.herokuapp.com/api/profile/me';
+            const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFlM2Y1ZDYwMGJlMTAwMTgzYTg2OWMiLCJpYXQiOjE3MDU5MTgzMDEsImV4cCI6MTcwNzEyNzkwMX0.oC8mhZ_YldjX2-Ab-I6p9knSGsc-L2IlVxX95iBN73o';
+
+            try {
+                const response = await axios.get(endpoint, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+
+                setProfile(response.data);
+            } catch (error) {
+                console.error('Errore nella richiesta:', error);
+            }
+        };
+        fetchProfile();
+    }, []);
+    if (!profile) {
+        return <div>Caricamento del profilo...</div>;
+    }
+
+    return (
+        <div>
+            <h1>Profilo</h1>
+            <p>Nome: {profile.name}</p>
+            <p>Cognome: {profile.surname}</p>
+            <p>Email: {profile.email}</p>
+            <p>Username: {profile.username}</p>
+            <p>Bio: {profile.bio}</p>
+            <p>Titolo: {profile.title}</p>
+            <p>Area: {profile.area}</p>
+            {profile.image && <img src={profile.image} alt="Profilo" />}
+        </div>
+    );
+};
+
+export default UserProfile;
