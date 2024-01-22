7125f8f0727fe22481fe3b8c5cef0f7e46005e4d
Casalena Antonio <casalenaantonio@hotmail.it>
2024-01-22 18:30:55 +0100

    modifiche

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import Formation from './components/Formation';
-import Activity from './components/Activity';
-import NavbarTop from './components/Navbar';
-import UserProfile from './components/UserProfile';
-import MainProfile from './components/MainProfile.jsx';
-import Interessi from './components/Interessi';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import Interessi from "./components/Interessi.jsx";
+import Formation from "./components/Formation";
+import Activity from "./components/Activity";
+import NavbarTop from "./components/Navbar";
+import UserProfile from "./components/UserProfile";
+import MainProfile from "./components/MainProfile.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DettaglioInteressi from "./components/DettaglioInteressi";
 
 function App() {
   return (
-    <div className="App">
-      <NavbarTop />
-      <UserProfile />
-    </div>
-
+    <Router>
+      <div className="App">
+        <NavbarTop />
+        <UserProfile />
+        <MainProfile />
+        <Formation />
+        <Activity />
+        <Interessi />
+        {/* Configura le route utilizzando Routes */}
+        <Routes>
+          {/* Altre route possono andare qui */}
+          <Route path="/risultati/:sezione" element={<DettaglioInteressi />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
