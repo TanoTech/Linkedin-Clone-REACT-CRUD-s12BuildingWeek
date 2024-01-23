72208fe4f73dca3b21f9d1931015838d24cdf787
GaetanoNapoli <gaetano.napoli@hotmail.com>
2024-01-23 09:45:40 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import React from "react";
 import NavbarTop from "./components/Navbar";
 import UserProfile from "./components/UserProfile";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DettaglioInteressi from "./components/DettaglioInteressi";
 import MayKnow from './components/MayKnow';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-
-    <div className="App">
-      <NavbarTop />
-      <UserProfile />
-      <MayKnow />
-      <Footer />
-    </div>
-
     <Router>
       <div className="App">
         <NavbarTop />
-        <UserProfile />
         <Routes>
+          <Route path="/" element={<UserProfile />} />
           <Route path="/risultati/:sezione" element={<DettaglioInteressi />} />
         </Routes>
+        <MayKnow />
+        <Footer />
       </div>
     </Router>
-
   );
 }
 
