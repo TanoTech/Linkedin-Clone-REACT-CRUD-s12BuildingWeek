a842b3cb052e0c93966adba27dc292f2a425929d
Matteo <matteo.3000@outlook.it>
2024-01-25 16:14:21 +0100

    pubblicità e fine create a post con immagini

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Modal, Form } from 'react-bootstrap';
-import { TbPhoto } from "react-icons/tb";
+import { TbPhoto } from 'react-icons/tb';
 
 const ImageUploader = ({ onSelectImage }) => {
   const [showModal, setShowModal] = useState(false);
@@ -11,30 +11,19 @@ const ImageUploader = ({ onSelectImage }) => {
 
     if (files && files.length > 0) {
       const file = files[0];
-      setSelectedImage(file);
+      setSelectedImage(URL.createObjectURL(file));
     }
-  }
+  };
 
   const handleImageSelection = () => {
     onSelectImage(selectedImage);
     setShowModal(false);
   };
 
-
-  const imageUrls = [
-    '/assets/imgPost/img1.jpeg',
-    '/assets/imgPost/img2.jpg',
-    '/assets/imgPost/img3.jpg',
-    '/assets/imgPost/img4.jpg',
-    '/assets/imgPost/img5.jpg',
-    '/assets/imgPost/img6.jpg',
-  ];
-
-
   return (
     <div>
       <Button variant="link" onClick={() => setShowModal(true)}>
-        <TbPhoto className='fs-4' alt="PhotoPicker" />
+        <TbPhoto className="fs-4" alt="PhotoPicker" />
       </Button>
 
       <Modal show={showModal} onHide={() => setShowModal(false)}>
@@ -45,13 +34,8 @@ const ImageUploader = ({ onSelectImage }) => {
         <Modal.Body>
           <Form>
             <Form.Group controlId="imageInput">
-              <Form.Control as="select" onChange={handleImageChange}>
-              {imageUrls.map(image => (
-                    <div key={image.value} onClick={() => handleImageSelection(image.value)}>
-                        <img src={image.src} alt={`Immagine ${image.value}`} />
-                    </div>
-                ))}
-              </Form.Control>
+              <Form.Control type="file" onChange={handleImageChange} accept="image/*" />
+              {selectedImage && <img src={selectedImage} alt="Selected" style={{ marginTop: '10px', width: '100%' }} />}
             </Form.Group>
           </Form>
         </Modal.Body>
