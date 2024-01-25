1a6191baf6b5c791627bcf7dc9092d19541779fd
GaetanoNapoli <gaetano.napoli@hotmail.com>
2024-01-25 23:03:14 +0100

    concluse tutte le funzionalità

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,46 +1,39 @@
-import "./css/CreatePost.css";
-import { Container } from "react-bootstrap"
-import { useContext } from "react"
-import { ProfileContext } from "../redux/contexts/ProfileContext"
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import React, { useState, useContext } from 'react';
+import { Container } from "react-bootstrap";
+import { ProfileContext } from "../redux/contexts/ProfileContext";
 
-const CreatePost = ({ newPostText, setNewPostText, createPost, posts }) => {
-    const { profile } = useContext(ProfileContext)
+const CreatePost = ({ newPostText, setNewPostText, createPost }) => {
+    const { profile } = useContext(ProfileContext);
+    const [postImage, setPostImage] = useState(null);
     const userProfileImg = profile ? profile.image : '';
 
+    const handleImageChange = (e) => {
+        setPostImage(e.target.files[0]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createPost(newPostText, postImage);
+        setNewPostText('');
+    };
+
     return (
-        <div className="createPostWrapper">
-            <div>
-                <form onSubmit={createPost}>
+        <Container className="createPostWrapper">
+            <form onSubmit={handleSubmit}>
                 <div className="searchBarPost">
                     <img className="ProfileImgPost" src={userProfileImg} alt="foto profilo utente" />
-                        <input
-                            value={newPostText}
-                            onChange={(e) => setNewPostText(e.target.value)}
-                            placeholder="Start a post"
-                        />
+                    <input
+                        value={newPostText}
+                        onChange={(e) => setNewPostText(e.target.value)}
+                        placeholder="Start a post"
+                    />
                 </div>
-                    <div className="iconWrapperPost">
-                        <div className="singleIconWrapperPost">
-                            <i className="bi bi-image-fill mediaIcon"></i>
-                            <p className="IconPost">Media</p>
-                        </div>
-    
-                        <div className="singleIconWrapperPost">
-                            <i className="bi bi-calendar3 calendarIcon "></i>
-                            <p className="IconPost">Event</p>
-                        </div>
-    
-                        <div className="singleIconWrapperPost">
-                            <i className="bi bi-pencil-square writeIcon"></i>
-                            <p className="IconPost">Write Article</p>
-                        </div>
-        
-                        <button type="submit">Send post</button>
-                    </div>
-                </form>
-            </div>
-            </div>
+                <input type="file" onChange={handleImageChange} />
+                <div className="iconWrapperPost">
+                    <button type="submit">Send post</button>
+                </div>
+            </form>
+        </Container>
     );
 };
 
