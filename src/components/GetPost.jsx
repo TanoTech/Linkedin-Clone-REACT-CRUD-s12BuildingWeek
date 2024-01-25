9ed434919331cd28cfdb00961c377ab3914010a4
MarcoV98 <148749261+MarcoV98@users.noreply.github.com>
2024-01-25 16:14:42 +0100

    commit 25/1

--- src/components/GetPost.jsx
+++ src/components/GetPost.jsx
@@ -6,7 +6,9 @@ const GetPost = ({ posts }) => {
         <div>
             {posts.map(data => (
                 <div key={data._id}>
-                    {data.image ? <img src={data.image} alt="post picture" /> : ''}
+                    {data.image ? <img 
+                    style={{width: '2rem' }}
+                    src={data.image} alt="post picture" /> : ''}
                     <p>{data.username}</p>
                     <p>{data.text}</p>
                 </div>
