72f0e48a9b375c1dedeb4e77a81a8fa2480e176e
GaetanoNapoli <gaetano.napoli@hotmail.com>
2024-01-26 16:33:50 +0100

    main

--- src/components/MyConnection.jsx
+++ src/components/MyConnection.jsx
@@ -37,32 +37,23 @@ const MyNetwork = () => {
                             connectionDetails.map(connection => (
 
                                 <Card key={connection._id} style={{ width: '12em' }} className='m-1'>
-
                                     <Card.Img className='mt-3 border' variant="top" src={connection.image} style={{ borderRadius: '50%', width: '5em', height: '5em', margin: '0 auto' }} />
-
                                     <Card.Body className='mt-auto ms-0'>
-
                                         <Link to={`/user/${connection._id}`} >
                                             <Card.Text style={{ height: '5em' }}>
-
                                                 <p className='fw-bold mb-0 text-center'>{connection.name} {connection.surname}</p>
                                                 <p className='mb-3 text-center'>{connection.title}</p>
-
                                             </Card.Text>
                                         </Link>
-
                                         <Button variant="primary" onClick={() => handleRemoveConnection(connection._id)}>Remove Connection</Button>
                                     </Card.Body>
-
                                 </Card>
-
                             ))
                         ) : (
                             <p>No connections yet.</p>
                         )}
                     </div>
                 </Col>
-
                 <Col md={4}>
                     <Ads />
                     <FooterHome />
