efeef03de7bf96496296de67a51805ce00ab0823
marcod08 <you@example.com>
2024-01-26 11:09:55 +0100

    aggiunta di una classe css su un component

--- src/components/OnThisPage.jsx
+++ src/components/OnThisPage.jsx
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap"
 
 const OnThisPage = () => {
     return (
-        <Container className="bg-white mt-3 rounded border border-solid p-2 pb-0">
+        <Container className="bg-white mt-3 rounded border border-solid p-2 pb-0 d-none d-md-block">
             <p className="m-0 mb-2 p-1" id="OnThisPageParagraph" style={{cursor:"pointer"}}>On this page</p>
             <p style={{cursor:"pointer"}}>People</p>
             <p style={{cursor:"pointer"}}>Post</p>
